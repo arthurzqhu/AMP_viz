b8b24d780eccf53fe179de8a47d93f00ce40d55d
Arthur <ols.Arthur@gmail.com>
2021-04-15 18:54:36 -0700

    update

--- case_dep_var.m
+++ case_dep_var.m
@@ -9,7 +9,7 @@ aer_dir_flags = [aer_dir.isdir];
 aer_dir_flags(1:2) = 0; % ignore the current and parent dir
 aero_N_str = {aer_dir(aer_dir_flags).name};
 
-w_dir = dir([output_dir,nikki,'/',mconfig,'/',upper('amp'),'_',...
+w_dir = dir([output_dir,nikki,'/',mconfig,'/',upper(ampORbin{1}),'_',...
     upper(bintype{1}),'/',aero_N_str{1},'/']);
 w_dir_flags = [w_dir.isdir];
 w_dir_flags(1:2) = 0; % ignore the current and parent dir
@@ -19,6 +19,6 @@ w_spd_str = {w_dir(w_dir_flags).name};
 
 % output dir for the figures
 plot_dir=['plots/' nikki '/' mconfig '/'];
-if ~exist(['plots/' nikki '/' mconfig],'dir')
-    mkdir(['plots/' nikki '/' mconfig])
+if ~exist(['plots/' nikki '/' mconfig '/'],'dir')
+    mkdir(['plots/' nikki '/' mconfig '/'])
 end
