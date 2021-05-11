f315d1e53b33a2a7da8d2503db4594175f4ec0b3
Arthur <ols.Arthur@gmail.com>
2021-05-10 20:38:39 -0700

    added more measure of goodness of fit: unweighted rsq and ratio of mean (as a measure of magnitude estimation)

--- wrsq.m
+++ wrsq.m
@@ -1,10 +1,34 @@
-function RR = wrsq(ya,yb,wgt)
+function [MRSQ, MR, RSQ] = wrsq(ya,yb,wgt)
+% calculate MRSQ modified rsq (mass weighted, bin as standard)
+% MD ratio of mean amp vs mean bin (accuracy of magnitude estimation)
+% RSQ regular rsq except mass weighted (accuracy of trend capturing)
+% ya = amp values
+% yb = bin values
+% wgt = weight, usually bin mass.
+
+ya=double(ya);
+yb=double(yb);
+wgt=double(wgt);
+
+if all(ya==0) || all(yb==0)
+   [MRSQ, MR, RSQ]=deal(nan);
+   return
+end
 
 vidx=~isnan(ya+yb);
 
-rss=sum((ya(vidx)-yb(vidx)).^2.*wgt);
-tss=sum((yb(vidx)-wmean(yb(vidx),wgt)).^2.*wgt);
+rss_m=sum((ya(vidx)-yb(vidx)).^2.*wgt);
+tss_m=sum((yb(vidx)-wmean(yb(vidx),wgt)).^2.*wgt);
 
-RR=1-rss/tss;
+MRSQ=1-rss_m/tss_m;
+
+% --------------
+
+MR=mean(ya)/mean(yb);
+
+% --------------
+
+mdl = fitlm(ya,yb);
+RSQ=mdl.Rsquared.Ordinary;
 
 end
