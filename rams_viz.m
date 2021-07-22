3ff09d804de305c30ecac197137b7cd0de054f65
Arthur Hu <ols.Arthur@gmail.com>
2021-07-21 22:04:43 -0700

    now uses rcp and rrp instead of diagnosed moments

--- rams_viz.m
+++ rams_viz.m
@@ -14,17 +14,20 @@ nfile=length(dir([outdir '*g1.h5']));
 fn={dir([outdir '*g1.h5']).name}; % get a file name and load the data info
 dat_info=h5info([outdir fn{1}]).Datasets;
 
-rams_hdf5c({'DMOMC3','DMOMR3'},0:nfile-1,outdir)
+rams_hdf5c({'RCP','RRP'},0:nfile-1,outdir)
 rams_hdf5c({'GLAT','GLON'},0,outdir)
-liqpath=squeeze((sum(runs.DMOMC3,3)+sum(runs.DMOMR3,3))*pio6rw);
+
+deltaz=100;
+%liqpath=deltaz*squeeze((sum(runs.DMOMC3,3)+sum(runs.DMOMR3,3))*pio6rw);
+liqpath=deltaz*squeeze((sum(runs.RCP,3)+sum(runs.RRP,3)));
 
 %%
 set(0, 'DefaultFigurePosition', [233 247 400 400])
 for it=1:size(liqpath,3)
-   contourf(runs.GLON,runs.GLAT,squeeze(liqpath(:,:,it)))
+   contourf(runs.GLON,runs.GLAT,squeeze(liqpath(:,:,it)),'LineColor','none')
    colorbar
    colormap(cmap.Blues)
-   caxis([1e-6 1e-1])
+   caxis([1e-6 1e0])
    set(gca,'ColorScale','log')
    
    xlabel('long')
