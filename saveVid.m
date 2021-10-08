075168a3c30e78261fd80984ea4a25e678b75d9f
Arthur Hu <ols.Arthur@gmail.com>
2021-10-08 14:09:46 -0700

    automatically detects what system for saveVid

--- saveVid.m
+++ saveVid.m
@@ -1,6 +1,11 @@
 function saveVid(F,filename,framerate)
 
-v=VideoWriter(['vids/' filename],'MPEG-4');
+if strcmp(computer('arch'),'maci64')
+   v=VideoWriter(['vids/' filename],'MPEG-4');
+elseif strcmp(computer('arch'),'glnxa64')
+   v=VideoWriter(['vids/' filename],'Archival');
+end
+
 v.FrameRate=framerate;
 open(v)
 writeVideo(v,F)
