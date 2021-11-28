618f1cd2991f61c91b900a1e51c20c7d39063fef
Arthur Hu <ols.Arthur@gmail.com>
2021-11-28 05:16:16 -0800

    gets rid of case dependence

--- loadnc.m
+++ loadnc.m
@@ -1,26 +1,23 @@
-function [filemeta,filename,fileinfo,var_name,stct]=loadnc(mp_in,case_interest)
+function [filemeta,filename,fileinfo,var_name,stct]=loadnc(mp_in)
 
-global ivar1 ivar2 its ici nikki mconfig output_dir case_list_str vnum ...
+global ivar1 ivar2 its nikki mconfig output_dir vnum ...
    bintype var1_str var2_str indvar_name_set indvar_name ...
    indvar_units indvar_ename indvar_ename_set indvar_units_set ...
    indvar2D_name indvar2D_units indvar2D_ename indvar2D_name_set ...
    indvar2D_ename_set indvar2D_units_set
 
-for ici = 1:length(case_interest)
-   icase=case_interest(ici);
-   
-   filemeta = dir([output_dir,nikki,'/',mconfig,'/',upper(mp_in),'_',...
-      upper(bintype{its}),'/',var1_str{ivar1},...
-      '/', var2_str{ivar2},'/KiD_m-amp_b-',lower(mp_in),'+',...
-      bintype{its},'_u-Adele_c-0',case_list_str{icase},...
-      '_v-',vnum,'.nc']);
-   filename = [filemeta.folder, '/', filemeta.name];
-   fileinfo = ncinfo(filename);
-   
-   for ivar = 1:length(fileinfo.Variables)
-      var_name{ivar,1} = fileinfo.Variables(ivar).Name;
-      stct.(var_name{ivar}) = ncread(filename, var_name{ivar});
-   end
+filedir=[output_dir,nikki,'/',mconfig,'/',upper(mp_in),'_',...
+   upper(bintype{its}),'/',var1_str{ivar1},...
+   '/', var2_str{ivar2},'/KiD_m-amp_b-',lower(mp_in),'+',...
+   bintype{its},'_u-Adele_c-0*_v-',vnum,'.nc'];
+
+filemeta = dir(filedir);
+filename = [filemeta.folder, '/', filemeta.name];
+fileinfo = ncinfo(filename);
+
+for ivar = 1:length(fileinfo.Variables)
+   var_name{ivar,1} = fileinfo.Variables(ivar).Name;
+   stct.(var_name{ivar}) = ncread(filename, var_name{ivar});
 end
 
 
