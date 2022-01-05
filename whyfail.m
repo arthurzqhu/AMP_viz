c81398d4d3d7c105af4d497d2196aff0bcc4ab2f
Arthur <ols.Arthur@gmail.com>
2022-01-04 23:38:03 -0800

    adds cloud and rain differentiation

--- whyfail.m
+++ whyfail.m
@@ -8,13 +8,6 @@ run global_var.m
 load([nikki '_fullmic_pfm'])
 load([nikki '_fullmic_success_rate'])
 
-mconfig_ls_dir=dir([output_dir,nikki,'/']);
-mconfig_ls_dir_flags=[mconfig_ls_dir.isdir];
-mconfig_ls_dir_flags(1:2)=0; % ignore the current and parent dir
-mconfig_ls={mconfig_ls_dir(mconfig_ls_dir_flags).name};
-
-iconf=2;
-mconfig=mconfig_ls{iconf};
 
 %%
 close all
@@ -25,10 +18,15 @@ vare=length(indvar_name);
 for its=1:length(bintype)
    for ivar=vars:vare
       
+      sr_c=squeeze(success_rate_c(its,:,:));
       sr_r=squeeze(success_rate_r(its,:,:));
       mrat=pfm.(indvar_name{ivar}).(bintype{its}).mr;
-      cov_mtx=nancov(sr_r(:),mrat(:));
-      cov_scl_c(ivar,its)=cov_mtx(1,2);
+      cov_mtx_r=nancov(sr_r(:),mrat(:));
+      cov_scl_r(ivar,its)=cov_mtx_r(1,2);
+      
+      cov_mtx_c=nancov(sr_c(:),mrat(:));
+      cov_scl_c(ivar,its)=cov_mtx_c(1,2);
+      
       figure
 %       refline(0,1); hold on
       scatter(sr_r(:),mrat(:))
