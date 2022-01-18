c84cebed72699ea670d3895a536310a8c78eac47
Arthur <ols.Arthur@gmail.com>
2022-01-18 00:34:18 -0800

    changes 2D profiles fig

--- p1_fig2_3.m
+++ p1_fig2_3.m
@@ -78,53 +78,54 @@ exportgraphics(gcf,'plots/p1/fig2.jpg','Resolution',300)
 % print(gcf,'plots/p1/fig2','-dpng','-r300')
 
 
-%%% fig 3 read files
-%twoDcase_meta=dir('./cases for plot/2D case');
-%twoDcasename = [twoDcase_meta.folder, '/', twoDcase_meta.name];
-%twoDcaseinfo = ncinfo(twoDcasename);
-%
-%for ivar = 1:length(twoDcaseinfo.Variables)
-%   var_name{ivar,1} = twoDcaseinfo.Variables(ivar).Name;
-%   twoD_struct.(var_name{ivar}) = ncread(twoDcasename, var_name{ivar});
-%end
-%
-%%% figure 3 plot
-%
-%t=twoD_struct.time;
-%x=twoD_struct.x;
-%z=twoD_struct.z;
-%w=twoD_struct.w;
-%w(w==-999)=nan;
-%
-%% fix the discontinuity!!
-%w(:,46,:)=0;
-%w(:,135,:)=0;
-%
-%qv=twoD_struct.vapor;
-%qv(qv==-999)=nan;
-%
-%figure('position',[1254 587 1307 560])
-%tl=tiledlayout(4,2);
-%nexttile(1,[3,1])
-%nanimagesc(x,z,squeeze(w(90,:,:))')
-%cbar=colorbar;
-%colormap(coolwarm_s)
-%caxis([-max(w(:)),max(w(:))])
-%cbar.Label.String='w [m/s]';
-%xlabel('x [m]')
-%ylabel('z [m]')
-%set(gca,'fontsize',16)
-%
-%nexttile(2,[4,1])
-%plot(1e3*squeeze(qv(1,2,:)),z,'linewidth',2)
-%xlabel('Specific humidity [g/kg]')
-%ylabel('Altitude [m]')
-%set(gca,'fontsize',16)
-%
-%nexttile
-%plot(t,max(w,[],[2,3]),'linewidth',2)
-%xlim([0 3600])
-%xlabel('Time [s]')
-%ylabel('Max w [m/s]')
-%set(gca,'fontsize',16)
-%exportgraphics(gcf,'plots/p1/fig3.jpg','Resolution',300)
+%% fig 3 read files
+twoDcase_meta=dir('./cases for plot/2D case');
+twoDcasename = [twoDcase_meta.folder, '/', twoDcase_meta.name];
+twoDcaseinfo = ncinfo(twoDcasename);
+
+for ivar = 1:length(twoDcaseinfo.Variables)
+  var_name{ivar,1} = twoDcaseinfo.Variables(ivar).Name;
+  twoD_struct.(var_name{ivar}) = ncread(twoDcasename, var_name{ivar});
+end
+
+%% figure 3 plot
+close all
+t=twoD_struct.time;
+x=twoD_struct.x;
+z=twoD_struct.z;
+w=twoD_struct.w;
+w(w==-999)=nan;
+
+% % fix the discontinuity!!
+% w(:,46,:)=0;
+% w(:,135,:)=0;
+
+qv=twoD_struct.vapor;
+qv(qv==-999)=nan;
+
+figure('position',[1245 587 1060 560])
+tl=tiledlayout(4,3);
+nexttile(1,[3,2])
+nanimagesc(x,z,squeeze(w(90,:,:))')
+cbar=colorbar;
+% colormap(coolwarm_s)
+% caxis([-max(w(:)),max(w(:))])
+cbar.Label.String='w [m/s]';
+xlabel('x [m]')
+ylabel('z [m]')
+ylim([0 3000])
+set(gca,'fontsize',16)
+
+nexttile(3,[4,1])
+plot(1e3*squeeze(qv(1,2,:)),z,'linewidth',2)
+xlabel('Specific humidity [g/kg]')
+ylabel('Altitude [m]')
+set(gca,'fontsize',16)
+
+nexttile(10,[1,2])
+plot(t,max(w,[],[2,3]),'linewidth',2)
+xlim([0 3600])
+xlabel('Time [s]')
+ylabel('Max w [m/s]')
+set(gca,'fontsize',16)
+exportgraphics(gcf,'plots/p1/fig3.jpg','Resolution',300)
