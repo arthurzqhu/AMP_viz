ec0df238f7ee8d7b159d0bb9d5761f6a3c238453
Arthur Hu <ols.Arthur@gmail.com>
2022-03-23 22:38:20 -0700

    LWP threshold for calculating LWC profile (does not seem to do much for VOCALS tho)

--- rprofile_comp.m
+++ rprofile_comp.m
@@ -1,13 +1,13 @@
 clear
 clear global
 
-global nfile outdir runs mpdat mp_list imp dz z deltaz deltax
+global nfile outdir runs mpdat mp_list imp dz z deltaz deltax nikki
 close all
 
 cabin = load('VOCALS_CABIN/081019.mat');
 addpath('ramsfuncs/')
 
-nikki = '2022-03-22'
+nikki = '2022-03-19'
 run rglobal_var
 mp_list = {'bin_sbm' 'amp_sbm' 'bin_tau' 'amp_tau'};
 
@@ -28,7 +28,7 @@ for imp = 1:length(mp_list) % loop through microphysics engines
    dat_info = h5info([outdir fn{1}]).Datasets;
 
    rams_hdf5c({'GLAT','GLON'},0,outdir)
-   var_int_idx = 6; % CWP RWP LWP
+   var_int_idx = [3 6]; % 3: LWP (for checking the threshold for cloudiness), 6: LWC
    idx = 1;
    for ivar = var_int_idx
       var_interest(idx) = ramsvar(var_name_set{ivar}, var_ename_set{ivar},...
@@ -38,22 +38,26 @@ for imp = 1:length(mp_list) % loop through microphysics engines
    rvar2phys(var_interest, 0)
 
    dz = deltaz;
-   z = z(2:end-1);
+   z_trim = z(2:end-1);
 
-   sim_lwc_prof = zeros(size(z));
-   cab_lwc_prof = zeros(size(z));
+   sim_lwc_prof = zeros(size(z_trim));
+   cab_lwc_prof = zeros(size(z_trim));
 
-   for iz = 1:length(z)
-      zi = z(iz);
+   vidx_rams_3d = mpdat(imp).(mp_list{imp}).LWP > thhd.cwp_th(1); % (x, y, t) 50 50 72
+
+   for iz = 1:length(z_trim)
+      zi = z_trim(iz);
       vidx_vocals = cabin.s_ap >= zi-dz/2 & cabin.s_ap < zi+dz/2;
-      vidx_rams = iz + 1; % since I got rid of the first and the last indices in z
-
-      sim_lwc_prof(iz) = 1e3*nanmean(mpdat(imp).(mp_list{imp}).LWC(:,:,vidx_rams,:),'all');
+      iz_rams = z==z(iz+1); % since I got rid of the first and the last indices in z
+      vidx_4d_slice = false(size(mpdat(imp).(mp_list{imp}).LWC));
+      vidx_4d_slice(:,:,iz_rams,:) = vidx_rams_3d;
+      sim_lwc_prof(iz) = nanmean(mpdat(imp).(mp_list{imp}).LWC(vidx_4d_slice),'all');
       cab_lwc_prof(iz) = nanmean(cabin.s_lwc_pdi(vidx_vocals));
    end
+
    nexttile
-   plot(cab_lwc_prof, z, 'displayname', 'obs'); hold on
-   plot(sim_lwc_prof, z, 'displayname', 'sim')
+   plot(cab_lwc_prof, z_trim, 'displayname', 'obs'); hold on
+   plot(sim_lwc_prof, z_trim, 'displayname', 'sim')
    grid
    legend('show')
    xlabel('LWC [g/kg]')
@@ -61,7 +65,7 @@ for imp = 1:length(mp_list) % loop through microphysics engines
    title(mps, 'Interpreter', 'none')
 end
 
-% assuming there's I'm only plotting LWC for now...
-print(['plots/rams/' var_name_set{var_int_idx} '_prof_' mconfig '.png'],'-dpng','-r300')
+% pretending there's I'm only plotting LWC for now...
+print(['plots/rams/LWC_prof_' mconfig '.png'],'-dpng','-r300')
 
 end
