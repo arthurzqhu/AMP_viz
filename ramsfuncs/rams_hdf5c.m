768ab2fc6864e6fab02af696c5bcfe89fca1ac99
Arthur Hu <ols.Arthur@gmail.com>
2022-03-23 22:31:46 -0700

    now adds deltax and deltaz to the global variable upon loading

--- ramsfuncs/rams_hdf5c.m
+++ ramsfuncs/rams_hdf5c.m
@@ -10,21 +10,21 @@ function rams_hdf5c(var,hr,fold,varargin)
 
 %tic
 
-%pref='~/rams6_bin/bin/';
-%pref='~/Desktop/rams6_bin/bin/';
-%pref='G:\\CopyHDD\';
-pref=[];
+%pref = '~/rams6_bin/bin/';
+%pref = '~/Desktop/rams6_bin/bin/';
+%pref = 'G:\\CopyHDD\';
+pref = [];
 
 if iscell(fold)
-    folder=fold;
+    folder = fold;
 elseif ischar(fold)
-    folder={fold};
+    folder = {fold};
 else
     'Error: please enter a valid folder format'
 end
     
 if ~iscell(var)
-    var={var};
+    var = {var};
 end
 
 if numel(varargin)>0
@@ -33,71 +33,75 @@ else
     cb = [2 139]; %start and length
 end
 
-global runs filepath z
+global runs filepath z deltax deltaz
 
 
-for f=1:length(folder)  
+for f = 1:length(folder)  
 %    folder{f}
-    files=dir([pref,folder{f},'/a-L*g1.h5']);
-    headfile=dir([pref,folder{f},'/a-L*head.txt']);
-    headfile=[headfile(1).folder '/' headfile(1).name];
+    files = dir([pref,folder{f},'/a-L*g1.h5']);
+    headfile = dir([pref,folder{f},'/a-L*head.txt']);
+    headfile = [headfile(1).folder '/' headfile(1).name];
 
     % get z-axis
-    hf_content=readcell(headfile);
-    zidx=find(strcmp(hf_content,'__ztn01'));
-    z_length=cell2mat(hf_content(zidx+1));
-    z=cell2mat(hf_content(zidx+2:zidx+z_length+1));
+    hf_content = readcell(headfile);
+    deltax_idx = find(strcmp(hf_content,'__deltaxn'));
+    deltax = cell2mat(hf_content(deltax_idx+2));
+    deltaz_idx = find(strcmp(hf_content,'__deltazn'));
+    deltaz = cell2mat(hf_content(deltaz_idx+2));
+    zidx = find(strcmp(hf_content,'__ztn01'));
+    z_length = cell2mat(hf_content(zidx+1));
+    z = cell2mat(hf_content(zidx+2:zidx+z_length+1));
 
-%     nd=0;
-%     for ff=1:length(files)
+%     nd = 0;
+%     for ff = 1:length(files)
 %         if ~strcmp(files(ff).name(18:21),'0000') & ~strcmp(files(ff).name(18:21),'1500') ...
 %                 & ~strcmp(files(ff).name(18:21),'3000') & ~strcmp(files(ff).name(18:21),'4500')
-%             nd=nd+1;
-%             delind(nd)=ff;
+%             nd = nd+1;
+%             delind(nd) = ff;
 %         end
 %     end
-    for v=1:length(var)
+    for v = 1:length(var)
 %        try
-        for t=1:length(hr)
-            filepath=[pref,folder{f},'/',files(hr(t)+1).name];
-            datapath=['/',upper(var{v})];
+        for t = 1:length(hr)
+            filepath = [pref,folder{f},'/',files(hr(t)+1).name];
+            datapath = ['/',upper(var{v})];
             %if strcmpi(var{v},'FFCD')
-            time_string=files(hr(t)+1).name(end-22:end-6);
-                test=single(h5read(filepath,datapath));
+            time_string = files(hr(t)+1).name(end-22:end-6);
+                test = single(h5read(filepath,datapath));
             %else
-            %    test=h5read(filepath,datapath,[1 1 cb(1)],[256 256 cb(2)]);
+            %    test = h5read(filepath,datapath,[1 1 cb(1)],[256 256 cb(2)]);
             %end
-            %info=h5info(filepath,datapath);
-            %ndim=length(info.ChunkSize);
+            %info = h5info(filepath,datapath);
+            %ndim = length(info.ChunkSize);
             dims = size(test);
             ndim = length(dims);
-            %nr=info.ChunkSize(1);
-            %nc=info.ChunkSize(2);
+            %nr = info.ChunkSize(1);
+            %nc = info.ChunkSize(2);
            
             %files(hr(t)+1).name
             
             %test=permute(test,[2 1 3 4]);
-            levs=size(test,3);
+            levs = size(test,3);
 
-            if v==1
-               if t==1
-                  time=zeros(length(hr),1);
+            if v == 1
+               if t == 1
+                  time = zeros(length(hr),1);
                end
-               time(t)=datenum(time_string,'yyyy-mm-dd-HHMMSS');
+               time(t) = datenum(time_string,'yyyy-mm-dd-HHMMSS');
             end
 
             if ndim <=3
-                if t==1
-                    A=zeros(size(test,1),size(test,2),size(test,3),length(hr));
-                    A=single(A);
+                if t == 1
+                    A = zeros(size(test,1),size(test,2),size(test,3),length(hr));
+                    A = single(A);
                 end
-                A(:,:,:,t)=test(:,:,:);
-            elseif ndim==4
-                if t==1
-                    A=zeros(dims(1),dims(2),levs,length(hr),33); % not compatible with tau
-                    A=single(A);
+                A(:,:,:,t) = test(:,:,:);
+            elseif ndim == 4
+                if t == 1
+                    A = zeros(dims(1),dims(2),levs,length(hr),33); % not compatible with tau
+                    A = single(A);
                 end
-                A(:,:,:,t,:)=test;
+                A(:,:,:,t,:) = test;
             end
         end % loop over t
 %         if numel(varargin)>0
@@ -109,15 +113,15 @@ for f=1:length(folder)
 %         elseif levs>1
 %             if ndim <=3
 %                 A=A(:,:,2:end,:);
-%             elseif ndim==4
+%             elseif ndim == 4
 %                 A=A(:,:,2:end,:,:);
 %             end
 %         end
         
-        if v==1
-           runs(f).time=time;
+        if v == 1
+           runs(f).time = time;
         end
-        runs(f).(var{v})=A;
+        runs(f).(var{v}) = A;
         
 %        catch
 %            'An error occurred'
