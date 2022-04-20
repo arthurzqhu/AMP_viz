041e02af6005dc32e83b16b5c5ffb43786c30554
Arthur <ols.Arthur@gmail.com>
2022-04-19 19:01:41 -0700

    output additional variable and update style

--- wrsq.m
+++ wrsq.m
@@ -1,48 +1,42 @@
-function [MR, RSQ, ER, MAXR] = wrsq(ya,yb,wgt)
+function [MR, RSQ, ER, MAXR, MD, SED] = wrsq(ya,yb,wgt)
 % calculate MRSQ modified rsq (mass weighted, bin as standard)
-% MD ratio of mean amp vs mean bin (accuracy of magnitude estimation)
+% MR ratio of mean amp vs mean bin (accuracy of magnitude estimation)
 % RSQ regular rsq except mass weighted (accuracy of trend capturing)
+% ER ratio of final amp vs bin
+% MAXR ratio of maximum of amp vs bin
+% MD difference between maximum of amp vs bin
+% SED difference between (ya(end) - ya(1)) and (yb(end) - yb(1))
 % ya = amp values
 % yb = bin values
 % wgt = weight, usually bin mass.
 
-ya=double(ya);
-yb=double(yb);
-wgt=double(wgt);
+ya = double(ya);
+yb = double(yb);
+wgt = double(wgt);
 
-if all(ya==0) || all(yb==0)
-   [MRSQ, MR, RSQ, ER, MAXR]=deal(nan);
+% ---------------------------------------------------
+vidxn = find(~isnan(ya + yb) & (ya ~= -999) & (yb ~= -999));
+
+if all(ya == 0) || all(yb == 0) || isempty(vidxn)
+   [MRSQ, MR, RSQ, ER, MAXR, MD, SED] = deal(nan);
    return
 end
 
-vidxn=find(~isnan(ya+yb) & (ya~=-999) & (yb~=-999));
+% ---------------------------------------------------
+MR = mean(ya(vidxn)) / mean(yb(vidxn));
+MD = mean(ya(vidxn)) - mean(yb(vidxn));
+ER = ya(vidxn(end)) / yb(vidxn(end));
+MAXR = max(ya(vidxn)) / max(yb(vidxn));
+SED = (ya(vidxn(end)) - ya(vidxn(1))) - (yb(vidxn(end)) - yb(vidxn(1)));
 
-%rss_m=sum((ya(vidxn)-yb(vidxn)).^2.*wgt);
-%tss_m=sum((yb(vidxn)-wmean(yb(vidxn),wgt)).^2.*wgt);
-
-%MRSQ=1-rss_m/tss_m;
-
-% --------------
-
-MR=mean(ya(vidxn))/mean(yb(vidxn));
-
-% --------------
-
-if length(ya)>1
-   mdl = fitlm(ya,yb);
-   RSQ=mdl.Rsquared.Ordinary;
+% ---------------------------------------------------
+if length(ya) > 1
+   mdl = fitlm(ya, yb);
+   RSQ = mdl.Rsquared.Ordinary;
 else
-   RSQ=nan;
+   RSQ = nan;
 end
 
-% --------------
-if ~isempty(vidxn)
-   ER = ya(vidxn(end))/yb(vidxn(end));
-   MAXR = max(ya(vidxn))/max(yb(vidxn));
-else
-   ER = nan;
-   MAXR = nan;
-end
-
+% ---------------------------------------------------
 
 end
