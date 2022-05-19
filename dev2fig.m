92d36cf1585baf1a193ae5886e530360a6d1b4da
Arthur <ols.Arthur@gmail.com>
2022-05-18 19:05:51 -0700

    skip plotting the arithmetic mean bars

--- dev2fig.m
+++ dev2fig.m
@@ -28,22 +28,22 @@ hold on
 b=bar(X,Y,1);
 b(1).FaceColor=color_order{1};
 b(2).FaceColor=color_order{2};
-b(1).FaceAlpha=0.50;
-b(2).FaceAlpha=0.50;
+b(1).FaceAlpha=0.75;
+b(2).FaceAlpha=0.75;
 b(1).LineWidth=1;
 b(2).LineWidth=1;
 b(1).BaseValue=1;
 b(1).BaseLine.Color=[.8 .8 .8];
 
-b_a=bar(X,Y_a,1);
-b_a(1).FaceColor=color_order{1};
-b_a(2).FaceColor=color_order{2};
-b_a(1).FaceAlpha=0.5;
-b_a(2).FaceAlpha=0.5;
-b_a(1).LineStyle=':';
-b_a(2).LineStyle=':';
-b_a(1).BaseValue=1;
-b_a(1).BaseLine.Color=[.8 .8 .8];
+% b_a=bar(X,Y_a,1);
+% b_a(1).FaceColor=color_order{1};
+% b_a(2).FaceColor=color_order{2};
+% b_a(1).FaceAlpha=0.5;
+% b_a(2).FaceAlpha=0.5;
+% b_a(1).LineStyle=':';
+% b_a(2).LineStyle=':';
+% b_a(1).BaseValue=1;
+% b_a(1).BaseLine.Color=[.8 .8 .8];
 
 hold off
 
