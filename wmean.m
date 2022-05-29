f8d465ed0c60490a3bcd386864ff97b1d4d8b7b3
Arthur <ols.Arthur@gmail.com>
2022-05-29 13:07:33 -0700

    bug fixes

--- wmean.m
+++ wmean.m
@@ -1,32 +1,32 @@
 function y = wmean(A,w)
+
 % y = wmean(A, w). A = random variable. w = weights.
 if any(w<0)
    %error('weights cant be negative')
-   warning('some weights are negative. they are set to 0 but might need to be inspected')
+   % warning('some weights are negative. they are set to 0 but might need to be inspected')
    w(w<0)=0;
+end
+
+% make sure they are shaped correctly
+if size(A, 1) == 1
+   A = A';
+end
+
+if size(w, 1) == 1
+   w = w';
 end
 
 w(isnan(A+w)) = 0;
 A(isnan(A+w)) = 0;
 
 if sum(w) ~= 1
-    w = w/nansum(w);
+   w = w/nansum(w);
 end
 
-if size(A,1) > size(A,2)
-    A = A';
+y = A'*w;
+
+if size(A, 1) ~= size(w, 1)
+   error('weights must have the same dimension as the data')
 end
-
-if size(A,2) == size(w,1)
-    y = A*w;
-elseif size(A,2) == size(w,2)
-    y = A*w';
-else
-    error('weights must have the same dimension as the data')
-end
-
-
-
-
 
 end
