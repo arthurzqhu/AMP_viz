2958c7912a80fa43ce5051251f63db447d43f58a
Arthur <ols.Arthur@gmail.com>
2022-07-25 01:58:43 -0700

    skips reading flag for bin output

--- get_varint.m
+++ get_varint.m
@@ -1,9 +1,14 @@
 function var_int = get_varint(int_idx)
 global var_name_set var_ename_set var_req_set var_unit_set var_range ...
-   var_linORlog l_da
+   var_linORlog l_da iab
 
 idx = 1;
 for ivar = int_idx
+
+   if iab == 2 && contains(var_name_set{ivar}, 'flag')
+      continue
+   end
+
    var_int(idx) = ramsvar(var_name_set{ivar}, var_ename_set{ivar},...
                                var_req_set{ivar}, var_unit_set{ivar}, ...
                                var_range{ivar}, var_linORlog{ivar}, l_da);
